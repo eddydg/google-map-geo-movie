7e0761e19c844956d67d44e45948beeae8a0d782
MeltedPenguin <diablo22007@gmail.com>
2016-12-04 18:52:52 +0100

    Static files in Express and update data when year changed

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var request = require("request");
 var pg = require('pg');
+var path = require('path');
 
 var express = require('express');
 var app = express();
 
 var connectionString = process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/geo';
 
+app.use('/public', express.static(path.join(__dirname, '/public')));
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: __dirname })
@@ -21,9 +23,16 @@ app.get('/movies/:year', function (req, res) {
     var year = req.params.year;
 
     const query = client.query(
-        "SELECT * FROM movies"
-        //" WHERE extract(year from to_timestamp(timestamp)) = " + year +
-        //" LIMIT 100"
+        "SELECT " +
+            "r.imdb_id," +
+            "r.title," +
+            "movies.year AS movie_year," +
+            "movies.votes AS total_votes," +
+            "r.votes AS votes_this_year," +
+            "r.year AS votes_year," +
+            "location, rank, genres, latlng" +
+        " FROM get_count_ratings_by_year("+year+") AS r" +
+        " JOIN movies ON movies.imdb_id = r.imdb_id"
     );
 
     var results = [];
