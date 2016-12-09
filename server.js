66e83e4aadbeeccf91e2a5a90a9597479835ec1e
MeltedPenguin <diablo22007@gmail.com>
2016-12-09 01:04:26 +0100

    Added Twit and Socketio support

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ var path = require('path');
 
 var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var socket = require('socket.io').listen(server);
+
+var Twit = require('twit');
+var twitterClient = new Twit({
+    consumer_key: 'FCAJfprhkxH113WXFjk2CSqwi',
+    consumer_secret: 'nmPTgEsf7hsJpPMm5fTSX9XIIMiz73mPH0q06GFPuOdlxpZ2CW',
+    access_token: '19071711-eySAafROa2CWdAK72ZLnYYJyHjxtrQ77Oowsz8YLo',
+    access_token_secret: '0wJomiJ5AKhulh1SVTFDmmrv4yVokq3Z6xnrVqcW4p1xQ'
+});
 
 var connectionString = process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/geo';
 
@@ -13,8 +23,8 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', {root: __dirname })
 })
 
-app.listen(3000, function () {
-    console.log('App listening on port 3000!')
+server.listen(3000, function () {
+    console.log('App listening on port 3000!');
 })
 
 app.get('/movies/:year', function (req, res) {
@@ -22,18 +32,18 @@ app.get('/movies/:year', function (req, res) {
     client.connect();
     var year = req.params.year;
 
-    const query = client.query(
-        "SELECT " +
-            "r.imdb_id," +
-            "r.title," +
-            "movies.year AS movie_year," +
-            "movies.votes AS total_votes," +
-            "r.votes AS votes_this_year," +
-            "r.year AS votes_year," +
-            "location, rank, genres, latlng" +
-        " FROM get_count_ratings_by_year("+year+") AS r" +
-        " JOIN movies ON movies.imdb_id = r.imdb_id"
-    );
+    const query = client.query(`
+        SELECT
+            r.imdb_id,
+            r.title,
+            movies.year AS movie_year,
+            movies.votes AS total_votes,
+            r.votes AS votes_this_year,
+            r.year AS votes_year,
+            location, rank, genres, latlng
+         FROM get_count_ratings_by_year(${year}) AS r
+         JOIN movies ON movies.imdb_id = r.imdb_id
+    `);
 
     var results = [];
     query.on('row', (row) => {
@@ -44,3 +54,33 @@ app.get('/movies/:year', function (req, res) {
         return res.json(results);
     });
 })
+
+var trackingMovie = null;
+var tweetStream = null;
+app.get('/track/:movie', function (req, res) {
+
+    if (req.params.movie) {
+        if (tweetStream) {
+            console.log("Stopped tweet stream on '"+trackingMovie+"'");
+            tweetStream.stop()
+        }
+
+        trackingMovie = req.params.movie;
+        console.log("Start tweet stream on '"+trackingMovie+"'");
+
+        var options = {
+            track: trackingMovie,
+            language: 'en'
+        }
+        tweetStream = twitterClient.stream('statuses/filter', options);
+
+        tweetStream.on('tweet', (tweet) => {
+            if (tweet.place) {
+                console.log("New tweet: " + tweet.text);
+                socket.emit('tweet', { tweet: tweet });
+            }
+        });
+    }
+
+    return res.end('Request ended');
+});
